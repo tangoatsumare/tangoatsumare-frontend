dd57121e5f7a17df832aaffa98d863bd1160322c
Dean <gomidean@gmail.com>
2022-09-08 20:28:18 +0900

    removed some comments

--- my-app/screens/camera.tsx
+++ my-app/screens/camera.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button, Image, Platform } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Platform } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 // import * as MediaLibrary from 'expo-media-library';
 // import Button from '../src/components/Button';
-// import { Button } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerTest() {
 
     const [image, setImage] = useState<string>(""); // typescript?
-    const [status, setStatus] = ImagePicker.useCameraPermissions();
+    // const [status, setStatus] = ImagePicker.useCameraPermissions();
 
     // const permissionCheck = async () => {
     //     await ImagePicker.getCameraPermissionsAsync();
@@ -49,93 +49,39 @@ export default function ImagePickerTest() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             {image && <Image source={{ uri: image }} style={{ width: 300, height: 300 }} />}
-            <Button title="Camera" onPress={openCamera} />
-            <Button title="Select image" onPress={pickImage} />
+            <Button icon="camera" onPress={openCamera}>
+                <Text>Open Camera</Text>
+            </Button>
+            <Button onPress={pickImage}>
+                <Text>Select An Image</Text>
+            </Button>
         </View>
     );
 }
 
 
-
-// export default function CameraTest() {
-
-//     const [hasCameraPermission, setHasCameraPermission] = useState(false);
-//     const [image, setImage] = useState(null);
-//     const [type, setType] = useState(CameraType.back);
-//     // const [flash, setFlash] = useState(Camera.Constants.FlashMode.off); //.off no work
-//     const cameraRef = useRef(null);
-//     // useEffect(() => {
-//     //     (async () => {
-//     //         MediaLibrary.requestPermissionsAsync();
-//     //         const cameraStatus = await Camera.requestCameraPermissionsAsync();
-//     //         // not sure if this will work
-//     //         if (cameraStatus.granted) {
-//     //             setHasCameraPermission(true);
-//     //         } else {
-//     //             alert("Camera Permission Denied!");
-//     //         }
-//     //     })();
-//     // }, []);
-
-//     const takePhoto = async () => {
-//         // MediaLibrary.requestPermissionsAsync();
-//         // const cameraStatus = await Camera.requestCameraPermissionsAsync();
-//         // // not sure if this will work
-//         // if (cameraStatus.granted) {
-//         //     setHasCameraPermission(true);
-//         // } else {
-//         //     alert("Camera Permission Denied!");
-//         // }
-//     };
-
-//     return (
-//         <View style={styles.container}>
-//             {/* <Button>Test</Button> */}
-//             <Camera
-//                 style={styles.camera}
-//                 type={type}
-//                 // flashMode={flash}
-//                 ref={cameraRef}
-//             >
-//                 <View style={styles.buttonContainer}>
-//                 <TouchableOpacity
-//                     style={styles.button}
-//                     onPress={takePhoto}>
-//                     <Text style={styles.text}>Take Photo</Text>
-//                 </TouchableOpacity>
-//                 </View>
-
-//                 <Text>Hello</Text>
-//             </Camera>
-//             <View>
-                
-//             </View>
-//         </View>
-//     )
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: 'pink',
-//         justifyContent: 'center',
-//         // alignItems: 'center',
-//     },
-//     camera: {
-//         flex: 1,
-//         borderRadius: 20,
-//     },
-//     buttonContainer: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     button: {
-//         width: 100,
-//         height: 100
-//     },
-//     text: {
-//         // width: 50,
-//         // height: 50
-//     }
-// })
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        justifyContent: 'center',
+        // alignItems: 'center',
+    },
+    camera: {
+        flex: 1,
+        borderRadius: 20,
+    },
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        width: 100,
+        height: 100
+    },
+    text: {
+        // width: 50,
+        // height: 50
+    }
+})
