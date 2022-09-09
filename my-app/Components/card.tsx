efe6cd93ec328e35f35bddcd14ee80b238907552
graememick@gmail.com <graememick@gmail.com>
2022-09-09 23:38:03 +0900

    feat: collection and single card display

--- my-app/Components/card.tsx
+++ my-app/Components/card.tsx
@@ -1,22 +1,64 @@
 import { useNavigation} from "@react-navigation/core";
 import { StackNavigationProp} from '@react-navigation/stack';
-import { ParamListBase } from '@react-navigation/native'
+import { ParamListBase, RouteProp } from '@react-navigation/native'
+import {Card, Paragraph, Title} from "react-native-paper";
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import axios from 'axios'
+import { useRoute } from '@react-navigation/core'
+import { ScreenRouteProp, StackParamsList } from "../library/routeProp";
 
-import React from "react";
-import {View, Text} from 'react-native'
-import {Button} from "react-native-paper";
 
-export const Card = () => {
+export const SingleCard = () => {
+
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+    const [flashcard, setFlashcard] = useState({})
 
-    return (
-        <View>
-            <Button icon="eye" mode="contained"
-                    onPress={()=>{
-                        navigation.navigate("Settings")
-                    }}>
-                <Text>To Settings</Text>
-            </Button>
-        </View>
-    )
+    const route = useRoute<RouteProp<Record<string, StackParamsList>, string>>();
+
+
+    useEffect(() => {
+
+      axios
+        .get(`https://tangoatsumare-api.herokuapp.com/api/flashcards/${route.params?.id}`)
+        .then((response) => {
+          setFlashcard(response.data[0])
+        });
+    }, []);
+  
+    const displayCard = (card: any) => {
+        return (
+            <Card key={card.target_word}>
+                    <Card.Content>
+                    <Card.Cover source={{ uri: 'https://www.japan-guide.com/g20/740/2040_04.jpg' }} />
+                    <Paragraph style={styles.text}>Reading: {card.reading}</Paragraph>
+                    <Title style={styles.text}>Vocab: {card.target_word}</Title>
+                    <Paragraph style={styles.text}> English: {card.english_definition}</Paragraph>
+                    <Paragraph style={styles.text}>Sentence: {card.context}</Paragraph>
+                    </Card.Content>
+                    
+               </Card>
+          
+
+               
+            );
+        };
+
+  return (
+    <View style={styles.container}>
+      {displayCard(flashcard)}
+            
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    text: {
+        textAlign: 'center',
+      }
+  })
--- my-app/Components/collection.tsx
+++ my-app/Components/collection.tsx
@@ -1,89 +1,89 @@
-// import { useNavigation} from "@react-navigation/core";
-// import { StackNavigationProp} from '@react-navigation/stack';
-// import { ParamListBase } from '@react-navigation/native'
-//
-// import React, { useEffect, useState } from "react";
-// import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
-// import axios from "axios";
-//
-// const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-// const [flashcards, setFlashcards] = useState([])
-//
-// useEffect(() => {
-//   axios
-//     .get("https://tangoatsumare-api.herokuapp.com//api/flashcards")
-//     .then((response: any) => {
-//       const flashcards =
-//         response.data;
-//       setFlashcards(flashcards)
-//     });
-// }, []);
-//
-//     const handleShowFlashcard = (flashcardName: any) => {
-//   //navigate
-//   console.log(flashcardName)
-//
-//       }
-//
-// export const Collection = () => {
-//
-//
-//     const displayFlashcard = (flashcards: readonly any[] | null | undefined) => {
-//         return (
-//             <FlatList
-//           ItemSeparatorComponent={() => <View style={styles.separator} />}
-//           data={flashcards}
-//           keyExtractor={(flashcard, index) => index.toString()}
-//           renderItem={({item}) => {
-//             return (
-//               <View key={item.name}>
-//
-//               <TouchableOpacity
-//                   onPress={() => {
-//                     handleShowFlashcard(item.name)
-//                   }}
-//                   style={styles.recipeButton}
-//                 >
-//                 <Text>Target Word: {item.target_word}</Text>
-//                 <Text>Context: {item.context}</Text>
-//                 </TouchableOpacity>
-//                </View>
-//             );
-//           }
-//         }
-//         />
-//         );
-//       }
-//
-//   return (
-//     <View style={styles.container}>
-//       {displayFlashcard(flashcards)}
-//     </View>
-//   )
-// }
-//
-// const styles = StyleSheet.create({
-//         button: {
-//             alignItems: 'center',
-//         },
-//         container: {
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//         },
-//         segment: {
-//
-//         },
-//           separator: {
-//             height: 1,
-//             backgroundColor: "grey"
-//           },
-//           recipeButton: {
-//             backgroundColor: '#white',
-//             width: '80%',
-//             padding: 5,
-//             borderRadius: 5,
-//             alignItems: 'center',
-//             marginTop: 5,
-//           }
-//     }
-// );
+import { useNavigation} from "@react-navigation/core";
+import { StackNavigationProp} from '@react-navigation/stack';
+import { StackParamsList } from "../library/routeProp";
+import {Button, Card, Paragraph, Title} from "react-native-paper";
+
+import React, { useEffect, useState } from "react";
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
+import axios from "axios";
+
+
+export const Collection = () => {
+
+  const navigation = useNavigation<StackNavigationProp<StackParamsList>>();
+const [flashcards, setFlashcards] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("https://tangoatsumare-api.herokuapp.com/api/flashcards")
+      .then((response: any) => {
+        const flashcards =
+          response.data;
+        setFlashcards(flashcards)
+      });
+  }, []);
+  
+      const handleShowFlashcard = (flashcardID: string) => {
+        navigation.navigate("Card", {id: flashcardID})
+        console.log(flashcardID)
+  
+        }
+
+    const displayFlashcard = (flashcards: readonly any[] | null | undefined) => {
+        return (
+            <FlatList
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          data={flashcards}
+          keyExtractor={(flashcard, index) => index.toString()}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity onPress={() => {
+                handleShowFlashcard(item._id)
+              }}
+            >
+              <Card key={item.target_word}>
+                    <Card.Content>
+                    <Card.Cover source={{ uri: 'https://www.japan-guide.com/g20/740/2040_04.jpg' }} />
+                    <Title style={styles.text}>Vocab: {item.target_word}</Title>
+                    <Paragraph style={styles.text}>Sentence: {item.context}</Paragraph>
+                    </Card.Content>
+                    <Card.Actions>
+             
+                </Card.Actions>
+               </Card>
+               <br/>                
+              </TouchableOpacity>
+            );
+          }
+        }
+        />
+        );
+      }
+
+  return (
+    <View style={styles.container}>
+      {displayFlashcard(flashcards)}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+        button: {
+            alignItems: 'center',
+        },
+        container: {
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        segment: {
+
+        },
+          separator: {
+            height: 1,
+            backgroundColor: "grey"
+          },
+          text: {
+            textAlign: 'center',
+          }
+    }
+);
