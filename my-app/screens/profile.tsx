a1f35f739095422e5c7a532ec465e13b7655730f
Dean <gomidean@gmail.com>
2022-09-13 20:03:06 +0900

    feat: impleted country flags, spacing between cards

--- my-app/screens/profile.tsx
+++ my-app/screens/profile.tsx
@@ -4,10 +4,10 @@ import { ParamListBase } from '@react-navigation/native'
 
 import React, { useState, useEffect } from "react";
 import {View, Text, StyleSheet, ScrollView, FlatList, TouchableOpacity} from 'react-native'
-import {Button, Divider, Avatar, Title, Card,} from "react-native-paper";
-// import CountryFlag from "react-native-country-flag";
+import {Button, Avatar, Title, Card,} from "react-native-paper";
+import CountryFlag from "react-native-country-flag";
 import axios from 'axios';
-import { Item } from "react-native-paper/lib/typescript/components/List/List";
+// import { Item } from "react-native-paper/lib/typescript/components/List/List";
 
 export const Profile = () => {
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -42,13 +42,14 @@ export const Profile = () => {
             nestedScrollEnabled
             // columnWrapperStyle={{justifyContent: 'space-evenly'}}
             // numColumns={2}
+            ItemSeparatorComponent={() => <View style={{width: 13}}/>}
             horizontal={true}
                 data={topTwo}
                 renderItem={({item}) => {
                     return (
                         <Card style={styles.card}>
                             <Card.Content>
-                                {/* <Card.Cover source={{uri: 'https://avatars.githubusercontent.com/u/96172124?v=4'}} /> */}
+                                {/* Need to change image cant be found source for line 53*/}
                                 <Card.Cover source={{uri: item["image"] ?  item["image"] : 'https://avatars.githubusercontent.com/u/96172124?v=4'}} />
                                 <Card.Title style={{backgroundColor: 'lightgray'}} title={item['target_word']} />
                             </Card.Content>
@@ -69,11 +70,10 @@ export const Profile = () => {
                 <View style={styles.profileBox}>
                     {/* <Text> profile area </Text> */}
                     <View style={styles.profileInfo}>
-                        <Text style={styles.text}>{tempUser.userName}</Text>
-                        {/* <Text style={styles.text}>Abraham Lincoln <CountryFlag isoCode="us" size={22}/></Text> */}
+                        <Text style={styles.text}>{tempUser.userName} <CountryFlag isoCode={tempUser.userCountry} size={22}/></Text>
                         <Text style={styles.text}>@{tempUser.userHandle}</Text>
                         <Text style={styles.text}>Currently learning: </Text>
-                        {/* <CountryFlag isoCode="jp" size={22} /> */}
+                        <CountryFlag isoCode={tempUser.userLearning} size={22} />
                     </View>
 
                     <View style={styles.avatarBox}>
@@ -120,46 +120,29 @@ export const Profile = () => {
 
 const styles = StyleSheet.create({
         profilePage: {
-            // flex: 1,
             alignContent: 'center',
-            // justifyContent: 'center',
-            // backgroundColor: '#f2eded',
         },
         profileBox: {
             flex: 0,
             flexDirection: 'row',
             justifyContent: 'space-around',
             alignItems: 'center',
-            // alignContent: 'center',
-            // borderColor: 'green',
-            // backgroundColor: 'blue',
             height: 200,
         },
         profileInfo: {
             alignContent: 'center',
-            // backgroundColor: 'green',
         },
         text: {
             fontSize: 16
         },
         avatarBox: {
-            // borderColor: 'black',
             flex: 0,
             alignContent: 'center',
-            // backgroundColor: 'yellow',
             borderWidth: 1,
             borderRadius: 100,
-            // margin: 0,
-            // marginLeft: 20,
         },
         cardBox: {
-            // display: 'flex',
-            // flexDirection: 'row',
-            // flexWrap: 'wrap',
-            // justifyContent: 'space-evenly',
             padding: 10,
-            // backgroundColor: 'red',
-            
         },
         card: {
             width: 180,
@@ -173,17 +156,6 @@ const styles = StyleSheet.create({
             textAlign: 'center',
             backgroundColor: '#f23535',
             color: 'white'
-        },
-        button: {
-            alignItems: 'center',
-        },
-        // container: {
-        //     flex: 1,
-        //     alignItems: 'center',
-        //     justifyContent: 'center',
-        // },
-        divider: {
-            backgroundColor: 'orange',
         },
     }
 );
