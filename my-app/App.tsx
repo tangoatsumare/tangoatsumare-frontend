96ae7f87504d4a6bd1ed299fe79822bb80a15c36
khiz125 <ken.hiro118@gmail.com>
2022-09-28 09:18:57 +0900

    feat: settimeout 10sec for splash

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,41 +1,67 @@
 // https://callstack.github.io/react-native-paper/1.0/getting-started.html
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { registerRootComponent } from 'expo';
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import {TabNav} from "./Components/tabNav";
-import {StackNav} from './Components/stackNavigator';
+import { StackNav } from './Components/stackNavigator';
 import 'react-native-gesture-handler';
 import { fontConfig } from "./library/fontConfig";
 import { LogBox } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 LogBox.ignoreAllLogs();
 
 export default function App() {
 
-    const theme = {
-        ...DefaultTheme,
-        roundness: 2,
-        version: 3,
-        colors: {
-            ...DefaultTheme.colors,
-            primary: '#FF4F4F',
-            secondary: '#1C1C1C',
-            tertiary: '#FFFFFF',
-        },
-        fonts: fontConfig // NOT WORKING YET
-    };
+  const [appIsReady, setAppIsReady] = useState(false);
 
-    return (
-        <SafeAreaProvider>
-        <PaperProvider theme={theme}>
-          <NavigationContainer independent={true}>
-              <StackNav />
-          </NavigationContainer>
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 10000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    version: 3,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#FF4F4F',
+      secondary: '#1C1C1C',
+      tertiary: '#FFFFFF',
+    },
+    fonts: fontConfig // NOT WORKING YET
+  };
+
+  return (
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer independent={true}>
+          <StackNav />
+        </NavigationContainer>
       </PaperProvider>
-        </SafeAreaProvider>
-    );
+    </SafeAreaProvider>
+  );
 }
 
 // https://docs.expo.dev/versions/latest/sdk/register-root-component/
